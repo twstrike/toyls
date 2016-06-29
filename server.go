f9458554f807e3011c10f4c650f8f48fbfaeec88
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-06-29 14:04:39 -0500

    Add serializeServerHello

--- server.go
+++ server.go
@@ -14,5 +14,23 @@ func deserializeServerHello(h []byte) (*serverHelloBody, error) {
 }
 
 func serializeServerHello(h *serverHelloBody) ([]byte, error) {
-	return nil, nil
+	compressionMethodLen := 1
+	sessionIDLen := len(h.sessionID)
+	vectorSizesLen := 4
+	capacity := protocolVersionLen + randomLen + cipherSuiteLen + compressionMethodLen + sessionIDLen + vectorSizesLen
+
+	hello := make([]byte, 0, capacity)
+	hello = append(hello, h.serverVersion.major, h.serverVersion.minor)
+
+	gmtUnixTime := writeBytesFromUint32(h.random.gmtUnixTime)
+	hello = append(hello, gmtUnixTime[:]...)
+	hello = append(hello, h.random.randomBytes[:]...)
+
+	hello = append(hello, byte(sessionIDLen))
+	hello = append(hello, h.sessionID...)
+
+	hello = append(hello, h.cipherSuite[:]...)
+	hello = append(hello, h.compressionMethod)
+
+	return hello, nil
 }
